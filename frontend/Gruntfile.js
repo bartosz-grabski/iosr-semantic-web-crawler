60931f0f391cb176259fc05abf293fa071adefbc
Bartosz Grabski <b.grabski92@gmail.com>
2015-05-18 01:39:55 +0200

    grunt support added

---
+++ frontend/Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-forever');
+
+    grunt.initConfig({
+
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        },
+        forever: {
+            app: {
+                options: {
+                    index: 'lib/app.js',
+                    logDir: 'logs'
+                }
+            }
+        }
+
+    });
+
+   grunt.registerTask('start', ['karma','forever:app:start'])
+
+};
