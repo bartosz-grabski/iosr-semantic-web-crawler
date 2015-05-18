18bd27dde4529749fdc7e3a692accbcda3d74276
Robert Skibinski <enedhilien@gmail.com>
2015-05-18 03:23:33 -0700

    Moved scrapy project

--- semantic-crawler/semanticCrawler/pipelines.py
+++ semantic-crawler/semanticCrawler/pipelines.py
@@ -6,6 +6,6 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class ScrapyspikePipeline(object):
+class SemanticcrawlerPipeline(object):
     def process_item(self, item, spider):
         return item
---
+++ semantic-crawler/semanticCrawler/settings.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for semanticCrawler project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'semanticCrawler'
+
+SPIDER_MODULES = ['semanticCrawler.spiders']
+NEWSPIDER_MODULE = 'semanticCrawler.spiders'
+
+ITEM_PIPELINES = [
+    'scrapy_mongodb.MongoDBPipeline',
+    ]
+
+MONGODB_URI = 'mongodb://localhost:27017'
+MONGODB_DATABASE = 'queries'
+MONGODB_COLLECTION = 'results'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'semanticCrawler (+http://www.yourdomain.com)'
