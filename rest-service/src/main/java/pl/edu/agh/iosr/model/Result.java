94476b7d177dc7cab853cb8f7248c3b4e1756957
Robert Skibinski <enedhilien@gmail.com>
2015-05-18 22:16:19 -0700

    Returning json results working

--- rest-service/src/main/java/pl/edu/agh/iosr/model/Result.java
+++ rest-service/src/main/java/pl/edu/agh/iosr/model/Result.java
@@ -1,17 +1,45 @@
 package pl.edu.agh.iosr.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.bson.types.ObjectId;
 import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Id;
-
-import java.util.ArrayList;
+import org.mongodb.morphia.annotations.Property;
 
 @Entity
 public class Result {
 
     @Id
     private ObjectId resultId;
-    private ObjectId queryId;
-    private ArrayList<Object> url;
+    @Property("query_id")
+    private String queryId;
+    @Property("url")
+    private String url;
 
+    @JsonProperty("result_id")
+    public String getResultId() {
+        return resultId != null ? resultId.toString() : null;
+    }
+
+    public void setResultId(ObjectId resultId) {
+        this.resultId = resultId;
+    }
+
+    @JsonProperty("query_id")
+    public String getQueryId() {
+        return queryId;
+    }
+
+    public void setQueryId(String queryId) {
+        this.queryId = queryId;
+    }
+
+    @JsonProperty("url")
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
