f9e38c183d15393fa074c2d0a9f93c8e3966bc25
Robert Skibinski <enedhilien@gmail.com>
2015-06-09 01:02:14 -0700

    Spider pig

--- rest-service/src/test/java/pl/edu/agh/iosr/ScrapyRunnerTest.java
+++ rest-service/src/test/java/pl/edu/agh/iosr/ScrapyRunnerTest.java
@@ -6,6 +6,6 @@ public class ScrapyRunnerTest {
 
     @Test
     public void test(){
-        ScrapyRunner.deployProject("superQuery", "http://wpdressing.com,http://anemo.pl");
+        ScrapyRunner.deployProject("superQuery", "http://wpdressing.com,http://anemo.pl", "diving,sailing");
     }
 }
