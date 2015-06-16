68b644bcbb9a444a1eb81832d1f20f4f645945f4
Robert Skibinski <enedhilien@gmail.com>
2015-06-15 22:43:42 -0700

    test fix

--- rest-service/src/test/java/pl/edu/agh/iosr/ScrapyRunnerTest.java
+++ rest-service/src/test/java/pl/edu/agh/iosr/ScrapyRunnerTest.java
@@ -2,10 +2,15 @@ package pl.edu.agh.iosr;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class ScrapyRunnerTest {
 
     @Test
     public void test(){
-        ScrapyRunner.deployProject("superQuery", "http://wpdressing.com,http://anemo.pl", "diving,sailing");
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("diving");
+        strings.add("sailing");
+        ScrapyRunner.deployProject("superQuery", "http://wpdressing.com,http://anemo.pl", strings);
     }
 }
